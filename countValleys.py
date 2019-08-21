2ca3d72ed18a989cc633355bbe89cb819ff759e6
Kerry Hill <Kerry@MacBook.hsd1.ca.comcast.net>
2019-08-21 13:40:39 -0700

    add file

---
+++ countValleys.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the countingValleys function below.
+def countingValleys(n, s):
+    height = 0
+    valleys = 0
+    for i in s:
+        # if you're below sea level and you're about to hit sea level increment valleys
+        if i == "U":
+            if height == -1:
+                valleys += 1
+            height += 1
+        elif i == "D":
+            height += -1
+    return(valleys)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    s = input()
+
+    result = countingValleys(n, s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
