533ca6e47da0abb5bb5e7ae7e91cb29b953cc1d8
Kerry <53912684+khill-turbo@users.noreply.github.com>
2019-08-23 15:42:39 -0700

    Update calcMeal.py

--- thirty-days/day2/calcMeal.py
+++ thirty-days/day2/calcMeal.py
@@ -9,17 +9,17 @@ import sys
 # Complete the solve function below.
 def solve(meal_cost, tip_percent, tax_percent):
 
-    #print(meal_cost)
+    # print(meal_cost)
 
-    tip_p = (meal_cost * (20/100) )
-    #print(tip_p)
+    tip_p = (meal_cost) * (tip_percent/100)
+    # print(tip_p)
 
-    tax_p = (meal_cost * (8/100) )
-    #print(tax_p)
+    tax_p = (meal_cost) * (tax_percent/100)
+    # print(tax_p)
 
-    total = (int(meal_cost+tip_p+tax_p))
-
+    total = (round(meal_cost+tip_p+tax_p))
     print(total)
+
 
 if __name__ == '__main__':
     meal_cost = float(input())
