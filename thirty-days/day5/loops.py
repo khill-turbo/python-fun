1b9cdee1221cef90038178eaa2a8f618d93c903d
Kerry <53912684+khill-turbo@users.noreply.github.com>
2019-08-25 17:32:24 -0700

    Create loops.py

---
+++ thirty-days/day5/loops.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    n = int(input())
+    i = 1
+    while (i <= 10):
+        total = (n*i)
+        print(n, " x ", i, " = ", total)
+        i += 1
