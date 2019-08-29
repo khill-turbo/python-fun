eea565e21000f0a32f02a9fdc15f8cc4c474541a
Kerry <53912684+khill-turbo@users.noreply.github.com>
2019-08-29 12:34:34 -0700

    Create dictionary.py

---
+++ thirty-days/day8/dictionary.py
@@ -0,0 +1,25 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    t = int(input())
+
+    thisdict = dict()
+    inputs = []
+    for i in range(t*2):
+        inputs.append(input())
+
+    for i in range(t):
+        thisdict.update(x.split(' ') for x in (inputs[i]).split(','))
+
+    holder = thisdict.keys()
+    for i in range(t,((t*2))):
+        if (inputs[i]) not in holder:
+            print("Not found")
+        else:
+            print((inputs[i])+"="+(thisdict.get(inputs[(i)])))
+
