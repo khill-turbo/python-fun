f2ca7371341f00c094cb165b2dc99f2bca174997
Kerry <53912684+khill-turbo@users.noreply.github.com>
2019-09-10 10:43:02 -0700

    Create inheritance.py

---
+++ thirty-days/day12/inheritance.py
@@ -0,0 +1,62 @@
+class Person:
+	def __init__(self, firstName, lastName, idNumber):
+		self.firstName = firstName
+		self.lastName = lastName
+		self.idNumber = idNumber
+	def printPerson(self):
+		print("Name:", self.lastName + ",", self.firstName)
+		print("ID:", self.idNumber)
+
+class Student(Person):
+    #   Class Constructor
+    #   
+    #   Parameters:
+    #   firstName - A string denoting the Person's first name.
+    #   lastName - A string denoting the Person's last name.
+    #   id - An integer denoting the Person's ID number.
+    #   scores - An array of integers denoting the Person's test scores.
+    #
+    # Write your constructor here
+
+    def __init__(self, firstName, lastName, idNumber, scores):
+        super().__init__(firstName, lastName, idNumber) 
+        self.scores = scores
+
+    #   Function Name: calculate
+    #   Return: A character denoting the grade.
+    #
+    # Write your function here
+    def calculate(self):
+        sumScores = int()
+        for i in self.scores:
+            #print("scores = ", self.scores)
+            #print("i = ", i)
+            sumScores += i
+        sumScores = round(sumScores/(len(self.scores)))
+        #print("rounded sumScores = ", sumScores)
+        stealth_check = {range(90, 101) : 'O', 
+        range(80, 89) : 'E',
+        range(70, 79) : 'A',
+        range(55, 69) : 'P',
+        range(40, 54) : 'D',
+        range(0, 39) : 'T'
+        }
+        # loop through the keys
+        for key in stealth_check:
+            # the keys are range(#, #) and not just the #
+            # so you need to search through the keys
+            if sumScores in key:
+                #print(stealth_check[key])
+                return(stealth_check[key])
+                break
+
+
+line = input().split()
+firstName = line[0]
+lastName = line[1]
+idNum = line[2]
+numScores = int(input()) # not needed for Python
+scores = list( map(int, input().split()) )
+s = Student(firstName, lastName, idNum, scores)
+s.printPerson()
+print("Grade:", s.calculate())
