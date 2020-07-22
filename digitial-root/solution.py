8ceddf5555648b9e4a1ed8358b8a88c2068876a3
Kerry <53912684+khill-turbo@users.noreply.github.com>
2020-07-21 18:27:15 -0700

    Remove print statements, improve variable names, add comments

--- digitial-root/solution.py
+++ digitial-root/solution.py
@@ -1,23 +1,20 @@
 def digital_root(n):
-    
+    # initialize listSummation in case n less than 9 and therefore while loop isn't entered
+    listSummation = n
     while n > 9:
-        #print(n)
-        myList = []
-        myString = str(n)
-        for i in myString:
-            myAdder = 0
-            myIntPartTwo = int(i)
-            myList.append(myIntPartTwo)
-            #print(myList)
-            #print("myAdder = ", myAdder)
-            for j in myList:
-                #print("j = ", j)
-                myAdder = (myAdder + j)
-            #print("myAdder = ", myAdder)
-        n = myAdder
+        # need to reset nList if n is still greater than one digit
+        nList = []
+        # which means you also need to reset nToString
+        nToString = str(n)
+        for i in nToString:
+            # need to reset listSummation every time you restart walk through the list
+            listSummation = 0
+            # add elements to the list
+            nList.append(int(i))
+            # create a summation of the elements in the list
+            for j in nList:
+                listSummation = (listSummation + j)
+        # need to set n to listSummation to get out of the while loop
+        n = listSummation
 
-    #print("myAdder = ", myAdder)
-    #print("n = ", n)
-    #print("myString = ", myString)
-    
-    return(myAdder)
+    return(listSummation)
